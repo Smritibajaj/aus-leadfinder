3d45b188d008358fde9bee95ea703df70debbaa9
Smritibajaj <my@pers.on.al>
2024-04-17 04:53:50 +0530

    changes for cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['http://localhost:5173/'],
-          credentials: true,
+    credentials: true,
   });
   await app.listen(8000);
   //Logger.log(`Listening on http://localhost:${port}`);
