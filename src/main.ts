c75227458d5ae47fe6532b81439b37ae15d38204
Smritibajaj <my@pers.on.al>
2024-04-17 05:03:22 +0530

    changes for cors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 //import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['http://localhost:5173/'],
+  // Enable CORS with specific options
+  const corsOptions: CorsOptions = {
+    origin: ['http://localhost:3000'], // Replace with your frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
-  });
+  };
+  app.enableCors(corsOptions);
   await app.listen(8000);
   //Logger.log(`Listening on http://localhost:${port}`);
 }
