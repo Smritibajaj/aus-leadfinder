1ecf9c5d045e75ae10f61893fde982f424fe2607
Smritibajaj <my@pers.on.al>
2024-04-17 04:28:20 +0530

    changes

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 //import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   await app.listen(8000);
   //Logger.log(`Listening on http://localhost:${port}`);
 }
