8dcce6a63e8794b474875b9db398984c01b896be
Smritibajaj <my@pers.on.al>
2024-04-17 09:25:32 +0530

    deployment file cors change

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 //import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Enable CORS with specific options
-  const allowedOrigin = 'localhost:5173';
-  const corsOptions: CorsOptions = {
-    origin: new RegExp(
-      '^(http|https)://(.+\\.)?' + allowedOrigin + '(:\\d{1,5})?/?$',
-      'i',
-    ),
-  };
-  app.enableCors(corsOptions);
+  app.enableCors({
+    // true for all origins
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+  });
   await app.listen(8000);
   //Logger.log(`Listening on http://localhost:${port}`);
 }
